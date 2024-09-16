bcc73a74adea41ae59e87e08d81fdfb1b99b4d3e
Lorenzo Gutierrez <37852325+lorenguti@users.noreply.github.com>
2024-09-15 21:13:16 -0300

    Initial project
    Proyecto inicial de Android Studio.

---
+++ app/src/main/java/utn/frba/mobile/moodmatch/MainActivity.kt
@@ -0,0 +1,47 @@
+package utn.frba.mobile.moodmatch
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import utn.frba.mobile.moodmatch.ui.theme.MoodMatchTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MoodMatchTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MoodMatchTheme {
+        Greeting("Android")
+    }
+}

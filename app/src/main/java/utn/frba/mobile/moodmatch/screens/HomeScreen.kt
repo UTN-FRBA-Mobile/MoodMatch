1b820fd8288b76ec006873d3218ca049c92c869f
ezequielformaggio <eformaggio@frba.utn.edu.ar>
2024-11-25 14:51:11 -0300

    feature/home se corrigen warings

--- app/src/main/java/utn/frba/mobile/moodmatch/screens/HomeScreen.kt
+++ app/src/main/java/utn/frba/mobile/moodmatch/screens/HomeScreen.kt
@@ -7,9 +7,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.ScrollableState
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
